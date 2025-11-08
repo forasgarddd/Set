5fe6ebdc65baef01f803f50edbedd81d1562d12f
forasgarddd <forbattlenetonly14@gmail.com>
2025-11-08 22:35:55 +0100

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Ivan Devitskyi on 08/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
